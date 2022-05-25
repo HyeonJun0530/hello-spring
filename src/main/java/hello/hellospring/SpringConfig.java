94972fdc89430f3a52d3437ae61c39592d6a07b5
hyeonjunkim <ksc6203@gmail.com>
2022-05-25 15:31:41 +0900

    스프링 입문

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,44 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+//    private DataSource dataSource;
+//    private EntityManager em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new JdbcMemberRepository(dataSource);
+////        return new MemoryMemberRepository();
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return JpaMemberRepository(em);
+//    }
+}
